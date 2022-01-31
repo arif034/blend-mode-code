cdb4e5dea0a893902618255cf4e8164a721a0da3
arif034 <45455917+arif034@users.noreply.github.com>
2022-01-31 23:28:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="a">
+        <div className="b"></div>
+      </div>
+      <div className="c"></div>
+      <div className="d"></div>
+    </div>
+  );
+}
